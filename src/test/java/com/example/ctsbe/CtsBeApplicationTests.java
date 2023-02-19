b05745a948b85bf69e5cd93e361e48cedfadeefa
bao.vuduc <baovdhe151408@gmail.com>
2023-02-19 17:26:14 +0700

    Add base folder

---
+++ src/test/java/com/example/ctsbe/CtsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ctsbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtsBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
