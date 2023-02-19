3d4656bbe7bdb1dd2c51008a266010048e829b5d
baototheboo <vuducbao.2000@gmail.com>
2023-02-19 17:26:14 +0700

    Add base folder

---
+++ src/main/java/com/example/ctsbe/CtsBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ctsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtsBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtsBeApplication.class, args);
+    }
+
+}
