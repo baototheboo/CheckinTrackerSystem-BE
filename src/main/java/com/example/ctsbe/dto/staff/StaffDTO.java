689fd98e78b80b9c9c83880ce56e6397271f3c29
hungl <hungle19052001@gmail.com>
2023-03-09 22:18:30 +0700

    hung fix crud group

--- src/main/java/com/example/ctsbe/dto/staff/StaffDTO.java
+++ src/main/java/com/example/ctsbe/dto/staff/StaffDTO.java
@@ -12,6 +12,6 @@ public class StaffDTO {
     private String fullName;
     private String dateOfBirth;
     private String phone;
-    private String promotionLevel;
+    private Integer promotionLevel;
     private String roleName;
 }
